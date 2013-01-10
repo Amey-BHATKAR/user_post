fe539ec880632ba503ed8c8b380d98ba2ac2327c
Bhatkar Amey <amey_summer2012@yahoo.com>
2013-01-10 19:20:18 +0100

    Sign up done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 UserPost::Application.routes.draw do
-  get "users/new"
 
+ resources :users
+
+  root to: 'static_pages#home'
+  #match '/user', to: 'static_pages#home'
+  match '/signup',  to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
